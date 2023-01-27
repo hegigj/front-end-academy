1cc6085d2d521cbeb750c1b620733b7ebd8781d3
hegigj <hgjoka16@gmail.com>
2023-01-27 20:53:44 +0100

    get countries and states

---
+++ address-api.js
@@ -0,0 +1,47 @@
+const cred = {
+    userEmail: 'hgjoka16@gmail.com',
+    userToken: '2n-__D3JK1ykprTvekJ5QWq3KIuTnnWyMISwqvAPx5z3E4FtzBfFUFlTDUpJTtQ4vzk'
+};
+
+const LOCALSTORAGE_ACCESS_TOKEN = 'user-form:access-token';
+
+async function getAccessTokenAPI() {
+    const api = 'https://www.universal-tutorial.com/api/getaccesstoken';
+    const { userToken, userEmail } = cred;
+    const fetchObjectResponse = await fetch(api, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'api-token': userToken,
+            'user-email': userEmail
+        }
+    });
+    const response = await fetchObjectResponse.json();
+    const { auth_token } = response;
+    localStorage.setItem(LOCALSTORAGE_ACCESS_TOKEN, auth_token);
+    return auth_token;
+}
+
+async function getCountriesAPI(accessToken) {
+    const api = 'https://www.universal-tutorial.com/api/countries';
+    const fetchObjectResponse = await fetch(api, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Bearer ' + accessToken
+        }
+    });
+    return await fetchObjectResponse.json();
+}
+
+async function getStatesAPI(accessToken, country) {
+    const api = 'https://www.universal-tutorial.com/api/states/' + country;
+    const fetchObjectResponse = await fetch(api, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Bearer ' + accessToken
+        }
+    });
+    return await fetchObjectResponse.json();
+}
---
+++ user-form.js
@@ -0,0 +1,50 @@
+const countrySelect = document.getElementById('country');
+const regionSelect = document.getElementById('region');
+
+let selectedCountry;
+
+getAccessTokenAPI();
+
+// async function add() {}
+// const add = async () => {};
+
+function fillOptions(selectElement, options) {
+    selectElement.innerHTML = '';
+    options.forEach((option) => {
+        const optionEl = document.createElement('option');
+        optionEl.value = option;
+        optionEl.innerText = option;
+        selectElement.appendChild(optionEl);
+    });
+}
+
+countrySelect.addEventListener('focus', async () => {
+    const accessToken = localStorage.getItem(LOCALSTORAGE_ACCESS_TOKEN);
+    if (accessToken) {
+        const countries = await getCountriesAPI(accessToken);
+        fillOptions(
+            countrySelect,
+            countries.map((country) => country.country_name)
+        )
+    } else {
+        alert('Countries could not be fetched!');
+    }
+});
+
+countrySelect.addEventListener('change', (event) => {
+    selectedCountry = event.target.value;
+    regionSelect.removeAttribute('disabled');
+});
+
+regionSelect.addEventListener('focus', async () => {
+    const accessToken = localStorage.getItem(LOCALSTORAGE_ACCESS_TOKEN);
+    if (accessToken && selectedCountry) {
+        const regions = await getStatesAPI(accessToken, selectedCountry);
+        fillOptions(
+            regionSelect,
+            regions.map((region) => region.state_name)
+        )
+    } else {
+        alert('Regions could not be fetched!');
+    }
+});
